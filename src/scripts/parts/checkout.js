f3eee2960ac965b01616fb6bea718ad9b42a7923
DeatCat3 <67590838+DeatCat3@users.noreply.github.com>
2021-02-21 17:22:23 +0300

    Update checkout.js

--- src/scripts/parts/checkout.js
+++ src/scripts/parts/checkout.js
@@ -1,10 +1,10 @@
 export default function checkout() {
 $('.error').hide();
-//Success
+
 function sendForm() {
   location.href='checkout-complete.html';
 }
-// Validate email
+
 function validateEmail(email) {
   var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
   return re.test(String(email).toLowerCase());
@@ -13,7 +13,7 @@ function validatePhone(tel) {
   var reg = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
   return reg.test(String(tel).toLowerCase());
 }
-// validate email and send form after success validation
+
 $(".inputs__bottom-price").on("click", validate);
 function validate() {
   var tel = $("#phone").val();
