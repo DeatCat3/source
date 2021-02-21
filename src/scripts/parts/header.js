2e352d7410cd66320221d5c397a2a1036331d648
DeatCat3 <67590838+DeatCat3@users.noreply.github.com>
2021-02-21 17:20:58 +0300

    Delete header.js

--- src/scripts/parts/header.js
+++
@@ -1,6 +0,0 @@
-export default function header() {
-    //$(function(){
-       // $(".arr-right, .arr-left").click(function(){
-       // $("header").css({'background-color': 'white'});
-        //});});
-}
